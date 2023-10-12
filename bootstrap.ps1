ef570ac770e51836d7567870ca61ac5184fdb86a
Carsha <matt@carsha.io>
2023-10-12 15:05:50 +1000

    Improve appx package handling and fix reg key behaviour if path doesn't exist

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -3,13 +3,14 @@
 ###############################
 $RegistryKeys = Import-Csv ".\registry-keys.csv"
 foreach ($Row in $RegistryKeys) {
-  Write-Host "Category:`t$($Row.Category)`nDescription:`t$($Row.Description)"
+  Write-Host "Setting registry key '$($Row.Category): $($Row.Description)' .."
+  if (-not(Test-Path -Path $Row.Path)) { New-Item -Path $Row.Path -Force | Out-Null }
   if ($Row.Type -eq 'Binary') {
-    $Value = $Row.Value.Split(",") | ForEach-Object { "0x$_"}
-    Set-ItemProperty -Path $Row.Path -Name $Row.Name -Type $Row.Type -Value ([byte[]]$Value) -Force  
+    $Value = $Row.Value.Split(",") | ForEach-Object { "0x$_" }
+    Set-ItemProperty -Path $Row.Path -Name $Row.Name -Type $Row.Type -Value ([byte[]]$Value) -Force | Out-Null
     continue
   }
-  Set-ItemProperty -Path $Row.Path -Name $Row.Name -Type $Row.Type -Value $Row.Value -Force
+  Set-ItemProperty -Path $Row.Path -Name $Row.Name -Type $Row.Type -Value $Row.Value -Force  | Out-Null
 }
 
 ###############################
@@ -32,14 +33,18 @@ $AppxPackages = @(
   "Microsoft.WindowsComunicationsApps",
   "Microsoft.SkypeApp"
 )
-foreach ($AppxPackage in $AppxPackages) {
-  Get-AppxPackage $AppxPackage | Remove-AppxPackage
+foreach ($Package in $AppxPackages) {
+  Write-Host "Removing package '$Package' .."
+  $PackageFullName = (Get-AppxPackage $Package ).PackageFullName
+  $PackageFullNamePro = (Get-AppxProvisionedPackage -Online | Where-Object { $_.Displayname -eq $App }).PackageName
+  if ($PackageFullName) { Remove-AppxPackage -Package $PackageFullName | Out-Null }
+  if ($PackageFullNamePro) { Remove-AppxProvisionedPackage -Online -PackageName $PackageFullNamePro | Out-Null }
 }
 
 ###############################
 # Services
 ###############################
-Get-Service DiagTrack,Dmwappushservice | Stop-Service | Set-Service -StartupType Disabled
+Get-Service DiagTrack, Dmwappushservice | Stop-Service | Set-Service -StartupType Disabled
 
 ############################
 # Chocolatey
@@ -48,9 +53,10 @@ Get-Service DiagTrack,Dmwappushservice | Stop-Service | Set-Service -StartupType
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 $ChocolateyPackages = Import-Csv ".\choco-packages.csv"
 foreach ($Row in $ChocolateyPackages) {
+  Write-Host "Installing package '$($Row.Name)' .."
   if ($Row.Parameters) {
-    choco install --confirm $Row.Name -params """$($Row.Parameters)"""
+    choco install --confirm $Row.Name -params """$($Row.Parameters)""" --limit-output
     continue
   }
-  choco install --confirm $Row.Name
+  choco install --confirm $Row.Name --limit-output
 }
